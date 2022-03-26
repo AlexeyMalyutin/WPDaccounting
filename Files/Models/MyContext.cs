2f5f35f28e5f283525204c2f28a8fa42c4a6e496
AlexeyMalyutin <alex.malyutin1301@gmail.com>
2022-03-26 20:12:18 +0300

    Добавьте файлы проекта.

---
+++ Files/Models/MyContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Files.Models
+{
+    public class MyContext: DbContext
+    {
+        public MyContext(DbContextOptions options)
+            : base(options) { }
+
+        public DbSet<FileModel> Files { get; set; }
+    }
+}
